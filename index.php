f55f10d2c634f162cea7877198b413b0b78c99ba
yann <yannlefranc330@gmail.com>
2023-05-11 16:07:39 +0200

    je me permet de vous transmettre ce code.

---
+++ index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.4/jquery.min.js"></script>
+    <script>
+        //permet de faire disparaitre
+        $(document).ready(function(){
+            $("p").click(function(){
+                $(this).hide();
+            });
+        });
+    </script>
+</head>
+<body>
+
+<p>thewitcher</p>
+<p>ciri</p>
+<p></p>
+
+</body>
+</html>
